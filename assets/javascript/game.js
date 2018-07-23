9ab6cb2873010709e64d8a82da5668619f957e20
Justin Riddle <justinriddle@Justins-iMac.local>
2018-07-23 13:45:10 -0700

    fixed javascript and jquery files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,89 @@
-function getNumber() {
-    var minNumber = 19; 
-    var maxNumber = 101; 
-    var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber);
-    $('#myNumber').html(randomnumber); 
-    return false; 
+$(document).ready(function(){
+  var Random=Math.floor(Math.random()*101+19)
+
+  $('#randomNumber').text(Random);
+
+  var num1= Math.floor(Math.random()*11+1)
+  var num2= Math.floor(Math.random()*11+1)
+  var num3= Math.floor(Math.random()*11+1)
+  var num4= Math.floor(Math.random()*11+1)
+
+  var userTotal= 0; 
+  var wins= 0;
+  var losses = 0;
+
+$('#numberWins').text(wins);
+$('#numberLosses').text(losses);
+
+function reset(){
+      Random=Math.floor(Math.random()*101+19);
+      console.log(Random)
+      $('#randomNumber').text(Random);
+      num1= Math.floor(Math.random()*11+1);
+      num2= Math.floor(Math.random()*11+1);
+      num3= Math.floor(Math.random()*11+1);
+      num4= Math.floor(Math.random()*11+1);
+      userTotal= 0;
+      $('#finalTotal').text(userTotal);
+      } 
+function win(){
+alert("Winner!");
+  wins++; 
+  $('#numberWins').text(wins);
+  reset();
 }
 
-window.onload = getNumber; 
-document.getElementById("yourNumber").innerHTML = randomnumber;
-
-function myCrystals() {
-    var x = Math.floor((Math.random() * 10) + 1);
-    document.getElementById(".crystal-image").innerHTML = x;
+function lose(){
+alert ("You lose :(");
+  losses++;
+  $('#numberLosses').text(losses);
+  reset()
 }
 
-var targetNumber = 50;
+  $('#one').on ('click', function(){
+    userTotal = userTotal + num1;
+    console.log("New userTotal= " + userTotal);
+    $('#finalTotal').text(userTotal); 
+        if (userTotal == Random){
+          win();
+        }
+        else if ( userTotal > Random){
+          lose();
+        }   
+  })  
+  $('#two').on ('click', function(){
+    userTotal = userTotal + num2;
+    console.log("New userTotal= " + userTotal);
+    $('#finalTotal').text(userTotal); 
+        if (userTotal == Random){
+          win();
+        }
+        else if ( userTotal > Random){
+          lose();
+        } 
+  })  
+  $('#three').on ('click', function(){
+    userTotal = userTotal + num3;
+    console.log("New userTotal= " + userTotal);
+    $('#finalTotal').text(userTotal);
 
-  $("#number-to-guess").text(targetNumber);
-
-  var counter = 0;
-  $(".crystal-image").on("click", function() {
-
-    counter += 10;
-
-    alert("New score: " + counter);
-
-    // Here we created some logic to "check" if the click counter matches the targetNumber.
-    // Remember, this click event will be triggered with each click.
-    // With each click the counter will increase by 10 and be re-evaluated against target.
-    if (counter === targetNumber) {
-
-      // If the numbers match we'll celebrate the user's win.
-      alert("You win!");
-    }
-
-  });
-
+          if (userTotal == Random){
+          win();
+        }
+        else if ( userTotal > Random){
+          loser();
+        } 
+  })  
+  $('#four').on ('click', function(){
+    userTotal = userTotal + num4;
+    console.log("New userTotal= " + userTotal);
+    $('#finalTotal').text(userTotal); 
+      
+          if (userTotal == Random){
+          win();
+        }
+        else if ( userTotal > Random){
+          lose();
+        }
+  });   
+}); 
